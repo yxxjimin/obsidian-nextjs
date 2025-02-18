e052f7c45e9ae91c379f4e01ac3149471e6d02c0
Jimin Yoo <jmy0819_@naver.com>
2025-02-18 11:22:32 +0900

    fix: disable react markdown

--- src/app/posts/components/renderer.tsx
+++ src/app/posts/components/renderer.tsx
@@ -1,16 +1,16 @@
-import ReactMarkdown from "react-markdown";
-import remarkGfm from 'remark-gfm';
-import rehypeRaw from 'rehype-raw';
-import components from "@/app/posts/components/mdx";
+// import ReactMarkdown from "react-markdown";
+// import remarkGfm from 'remark-gfm';
+// import rehypeRaw from 'rehype-raw';
+// import components from "@/app/posts/components/mdx";
 
-export default function MarkdownRenderer({ content }: { content: string | undefined }) {
-  return (
-    <ReactMarkdown 
-      remarkPlugins={[remarkGfm]} 
-      rehypePlugins={[rehypeRaw]} 
-      components={components}
-    >
-      {content}
-    </ReactMarkdown>
-  );
-}
+// export default function MarkdownRenderer({ content }: { content: string | undefined }) {
+//   return (
+//     <ReactMarkdown 
+//       remarkPlugins={[remarkGfm]} 
+//       rehypePlugins={[rehypeRaw]} 
+//       components={components}
+//     >
+//       {content}
+//     </ReactMarkdown>
+//   );
+// }
