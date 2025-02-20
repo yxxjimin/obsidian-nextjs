03bd77b7933bb707080535c1a40f5a5221d52b8a
Jimin Yoo <jmy0819_@naver.com>
2025-02-20 23:45:20 +0900

    chore: disable webpack cache

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (
+    config,
+    { dev }
+  ) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      })
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
