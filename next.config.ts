95e0166805292312e64f9e5e380e8ac9cc6436ca
Jimin Yoo <jmy0819_@naver.com>
2025-02-21 22:28:24 +0900

    chore: exclude webpack cache output file

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (
-    config,
-    { dev }
-  ) => {
-    if (config.cache && !dev) {
-      config.cache = Object.freeze({
-        type: 'memory',
-      })
-    }
-    return config;
+  // webpack: (
+  //   config,
+  //   { dev }
+  // ) => {
+  //   if (config.cache && !dev) {
+  //     config.cache = Object.freeze({
+  //       type: 'memory',
+  //     })
+  //   }
+  //   return config;
+  // }
+  outputFileTracingExcludes: {
+    '**/*': [
+      '.next/cache/webpack/**', 
+    ],
   }
 };
 
